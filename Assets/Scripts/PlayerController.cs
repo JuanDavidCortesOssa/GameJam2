c6ad4a8bed9ee5335bb8ed9ba045f050f05bc0c2
Andres Lara <andres-lh@outlook.com>
2022-11-11 17:50:18 -0500

    add boundarys to player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,29 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     private bool hasPowerUp = false;
-    
+    private float verticalBoundary;
+    private float horizontalBoundary;
+
+
     [Header("Ship Setup")]
     [SerializeField] private float speed;
     [SerializeField] float powerUpDuration;
-   
+
 
     [Space()]
     [Header("Ship's armament")]
     [SerializeField] private List<BulletManager> guns;
     [SerializeField] private GameObject shield;
 
+    [Space()]
+    [Header("Map reference")]
+    [SerializeField] GameObject background;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        verticalBoundary = background.GetComponent<MeshRenderer>().bounds.max.y;
+        horizontalBoundary = background.GetComponent<MeshRenderer>().bounds.max.x;
     }
 
     // Update is called once per frame
@@ -41,15 +49,32 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+
+        if (transform.position.y >= verticalBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, verticalBoundary, transform.position.z);
+        }
+        else if (transform.position.y <= -verticalBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, -verticalBoundary, transform.position.z);
+        }
+        else if (transform.position.x >= horizontalBoundary)
+        {
+            transform.position = new Vector3(horizontalBoundary, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= -horizontalBoundary)
+        {
+            transform.position = new Vector3(-horizontalBoundary, transform.position.y, transform.position.z);
+        }
     }
 
     private void Shoot()
     {
-        foreach(BulletManager gun in guns)
+        foreach (BulletManager gun in guns)
         {
-            if(gun.isActiveAndEnabled == true) gun.CreateBullet();
+            if (gun.isActiveAndEnabled == true) gun.CreateBullet();
         }
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -57,8 +82,7 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("PowerUp")) StartCoroutine(SetPowerUp());
         else if (other.CompareTag("Enemy") && !hasPowerUp) Destroy(gameObject);
 
-        Destroy(other.gameObject);
-
+        if (!other.CompareTag("PlayerBullet")) Destroy(other.gameObject);
     }
 
     private IEnumerator SetPowerUp()
@@ -72,14 +96,14 @@ public class PlayerController : MonoBehaviour
         hasPowerUp = false;
         SetPowerUpGuns(false);
         shield.SetActive(false);
-      
+
     }
 
     private void SetPowerUpGuns(bool activate)
     {
-        for(int i = 1; i < guns.Count; i++)
+        for (int i = 1; i < guns.Count; i++)
         {
-            if(activate) guns[i].gameObject.SetActive(true);
+            if (activate) guns[i].gameObject.SetActive(true);
             else guns[i].gameObject.SetActive(false);
         }
     }
