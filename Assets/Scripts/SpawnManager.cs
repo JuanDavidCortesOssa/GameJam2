3fb3409ce1b9423ff003260dca06324f1f1fa35d
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2022-11-12 17:53:40 -0500

    Background added and spawn manager improved

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,31 +4,51 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [Header("Boundaries")]
     [SerializeField] private Transform superiorBoundTransform;
     private Vector3 superiorPosition;
 
     [SerializeField] private Transform inferiorBoundTransform;
     private Vector3 inferiorPosition;
 
-    [SerializeField] private float spawnInterval;
-    [SerializeField] private List<GameObject> spawnList;
+    [Header("Enemies")]
+    [SerializeField] private float enemiesFirstSpawnTime;
+    [SerializeField] private float enemiesSpawnInterval;
+    [SerializeField] private List<GameObject> enemiesList;
+
+    [Header("Objects")]
+    [SerializeField] private float objectsFirstSpawnTime;
+    [SerializeField] private float objectsSpawnInterval;
+    [SerializeField] private List<GameObject> objectsList;
+
 
     private void Start()
     {
         superiorPosition = superiorBoundTransform.position;
         inferiorPosition = inferiorBoundTransform.position;
 
-        InvokeRepeating("SpawnGameObjects", 2f, spawnInterval);
+        InvokeRepeating("SpawnGameEnemies", enemiesFirstSpawnTime, enemiesSpawnInterval);
+        InvokeRepeating("SpawnGameObjects", objectsFirstSpawnTime, objectsSpawnInterval);
     }
 
     public void SpawnGameObjects()
     {
-        int randomObjectNumber = Random.Range(0, spawnList.Count);
+        int randomObjectNumber = Random.Range(0, objectsList.Count);
 
         Vector3 randomPosition = new(superiorPosition.x, 
             Random.Range(superiorPosition.y, inferiorPosition.y), superiorPosition.z);
 
-        Instantiate(spawnList[randomObjectNumber], randomPosition, spawnList[randomObjectNumber].transform.rotation);
+        Instantiate(objectsList[randomObjectNumber], randomPosition, objectsList[randomObjectNumber].transform.rotation);
+    }
+
+    public void SpawnGameEnemies()
+    {
+        int randomObjectNumber = Random.Range(0, enemiesList.Count);
+
+        Vector3 randomPosition = new(superiorPosition.x,
+            Random.Range(superiorPosition.y, inferiorPosition.y), superiorPosition.z);
+
+        Instantiate(enemiesList[randomObjectNumber], randomPosition, enemiesList[randomObjectNumber].transform.rotation);
     }
 
 }
