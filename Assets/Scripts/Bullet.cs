517273092e63d31036eadc9b0a813ad76be6bdce
Andres Lara <andres-lh@outlook.com>
2022-11-11 21:44:53 -0500

    destroy bullets after 1.5 seconds

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,6 +7,11 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float _bulletSpeed;
     public Vector3 direction;
 
+    void Start()
+    {
+        Destroy(gameObject, 1.5f);
+    }
+
     // Update is called once per frame
     void Update()
     {
